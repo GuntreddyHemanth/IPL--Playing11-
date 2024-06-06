47f47d8db414733105a6ba822b296f87de9ca2cb
GuntreddyHemanth <guntreddyHemanth@gamil.com>
2024-06-06 17:28:42 +0000

    save

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css'
 const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
+    <Route exact path="/team-matches/:id" component={TeamMatches} />
   </Switch>
 )
 
--- src/components/TeamMatches/index.js
+++ src/components/TeamMatches/index.js
@@ -1,1 +1,35 @@
-// Write your code here
+import {Component} from 'react'
+import Loader from 'react-loader-spinner'
+
+import './index.css'
+
+class TeamMatches extends Component {
+  state = {
+    isLoading: true,
+    recentMatchesData: {},
+  }
+
+  renderTeamMatches = () => {
+      const {match} = this.props
+      const {params} = match 
+      const {id} = params 
+      const {recentMatchesData} = this.state 
+      const {}
+  }
+
+  renderLoader = () => (
+    <div data-testid="loader" className="loader-container">
+      <Loader type="oval" color="#ffffff" height="50" />
+    </div>
+  )
+
+  render() {
+    const {isLoading} = this.state
+
+    return (
+      <div>{isLoading ? this.renderLoader() : this.renderTeamMatches()}</div>
+    )
+  }
+}
+
+export default TeamMatches
